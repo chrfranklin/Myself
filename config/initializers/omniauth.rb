e130fc038177b685872435702741acd075df437c
baobao <baobao@ben-N82JG.(none)>
2013-04-02 15:37:46 +0800

    facebook callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,3 +6,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :github, '2fd1de851974f42391da', 'b77afc9e152d02b1c2d866c8a80136ef5962e64a'
            #:scope => 'email,user_birthday,read_stream', :display => 'popup'
 end
+
+require 'sinatra/base'
+
+get '/' do
+  'This is callback.'
+end
